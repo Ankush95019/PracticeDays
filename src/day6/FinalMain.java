b826669b0b2362421787ac72c036d4c835b650b5
Ankush <ankush4031@gmail.com>
2025-07-25 15:15:33 +0530

    FinalKeyword program added in Day 6

---
+++ src/day6/FinalMain.java
@@ -0,0 +1,33 @@
+package day6;
+
+class FinalKeyword{
+	final void show() {
+		System.out.print("I am final show() method in base");
+	}
+}
+
+final class FinalKeywordSubClass extends FinalKeyword{
+//	void show() {
+//		System.out.print("I am final show() method in subclass"); // not allowed to override because this method is final
+//	}
+}
+
+
+//class FinalKeyword1 extends FinalKeywordSubClass{ // not valid because class is already final and can't be inherited
+
+//}
+
+public class FinalMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int twenty = 20;
+		// twenty = 25; // wrong can't assign new value 
+		System.out.println(twenty);
+		
+		FinalKeyword fk = new FinalKeyword();
+		fk.show();
+		
+	}
+
+}
