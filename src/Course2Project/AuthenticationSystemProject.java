5148462b3a0142c63d15bd6e4d9678f13330fe30
Ankush <ankush4031@gmail.com>
2025-07-29 17:27:02 +0530

    Activities added for Day9

---
+++ src/Course2Project/AuthenticationSystemProject.java
@@ -0,0 +1,34 @@
+package Course2Project;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class User{
+	private String username;
+	private String password;
+	
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	
+}
+
+public class AuthenticationSystemProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
