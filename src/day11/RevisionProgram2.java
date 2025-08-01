3c3799aa0333f67b1190cc210339336258fa7100
Ankush <ankush4031@gmail.com>
2025-08-01 13:25:54 +0530

    Activities added for Day12

--- src/day11/RevisionProgram2.java
+++ src/day11/RevisionProgram2.java
@@ -13,6 +13,19 @@ public class RevisionProgram2 {
          System.out.println(b++ * 8 + 3);
          System.out.println(c++);
          System.out.println(d);
+         System.out.println("Ceil:");
+         
+         double x = 3.14;  
+         int y = (int) Math.ceil(x);
+         int z = (int) Math.floor(x);
+         System.out.println(y);
+         System.out.println("floor "+z);
+         
+//         int[] num = {10,20,30,40};
+         int num[] = {10,20,30,40};
+         System.out.println(num[0]);
+         
+         
 	}
 
 }
